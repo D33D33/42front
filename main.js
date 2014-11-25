a4780a1e00aa036f0915b6de7ac237b124e278b7
deedee <k4georges@gmail.com>
2014-11-25 11:48:45 +0100

    http-proxy

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-var bouncy = require('bouncy');
+var http = require('http'),  
+    httpProxy = require('http-proxy');
 
-var server = bouncy(function (req, res, bounce) {
-    if (req.headers.host === 'print.ct') {
-	console.log('print.ct');
-        bounce(3000);
-    }
-    else if (req.headers.host === 'register.ct') {
-	console.log('register.ct');
-        bounce(3001);
-    }
-    else {
-	console.log('no host');
-        res.statusCode = 404;
-        res.end('no such host');
-    }
-});
-server.listen(8000);
+var options = {  
+  hostnameOnly: true,
+  router: {
+    'print.ct': '127.0.0.1:3000',
+    'login.ct': '127.0.0.1:3001'
+  }
+}
+
+var proxyServer = httpProxy.createServer(options).listen(8000); 
